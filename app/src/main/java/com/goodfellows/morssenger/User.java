837a4444356d80678f3ad8fea7672c271e6dba3e
evansstevenson <stevensonevans@gmail.com>
2019-11-13 13:26:17 -0700

    added code to User class body with getters and setters

--- app/src/main/java/com/goodfellows/morssenger/User.java
+++ app/src/main/java/com/goodfellows/morssenger/User.java
@@ -1,4 +1,50 @@
 package com.goodfellows.morssenger;
 
 public class User {
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+
+    User(){}
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 }
