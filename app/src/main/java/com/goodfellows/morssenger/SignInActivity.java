ec500cf161937db0c809955eb786aae0af624361
Ryan Bey <56265847+ryanbey@users.noreply.github.com>
2019-11-13 13:39:40 -0700

    navigation bar

--- app/src/main/java/com/goodfellows/morssenger/SignInActivity.java
+++ app/src/main/java/com/goodfellows/morssenger/SignInActivity.java
@@ -10,5 +10,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        getSupportActionBar().hide();
     }
 }
