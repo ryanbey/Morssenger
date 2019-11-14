f6b0f5bca2b015dcbe974dee371f6461f0bca563
evansstevenson <stevensonevans@gmail.com>
2019-11-14 13:38:51 -0700

    added code to LoginActivity and SignUpActivity to allow user to sign in and create an account using firebase. Also fixed bug that made program crash loop

--- app/src/main/java/com/goodfellows/morssenger/LoginActivity.java
+++ app/src/main/java/com/goodfellows/morssenger/LoginActivity.java
@@ -1,26 +1,98 @@
 package com.goodfellows.morssenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth auth; //creates fire base Authentication object -Evans
+    private FirebaseAuth.AuthStateListener authListener;
+    private Button loginBtn;
+    private EditText userEmail, userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_login);
-
+        userEmail = (EditText) findViewById(R.id.et_email);
+        userPassword = (EditText) findViewById(R.id.et_password);
+        loginBtn = (Button) findViewById(R.id.button_login);
         getSupportActionBar().hide();
+
+        //firebase - Evans
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+        loginBtn = (Button) findViewById(R.id.button_login);
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // launch conversation activity if user is logged in
+                    Intent existingUser = new Intent(LoginActivity.this, ConversationsActivity.class);
+                    startActivity(existingUser);
+                    finish();
+                }
+            }
+        };
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful())
+                                {
+                                    Toast.makeText(getApplicationContext(), "Wrong email/password!", Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    Intent intent = new Intent(LoginActivity.this, ConversationsActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
     }
 
-    public void displayConversationActivity(View view) {
-        Intent iConversation = new Intent(this, ConversationsActivity.class);
-        startActivity(iConversation);
-    }
+   // public void displayConversationActivity(View view) {
+    //    Intent iConversation = new Intent(this, ConversationsActivity.class);
+    //    startActivity(iConversation);
+   // }
 
     public void displayCreateAccount(View view) {
         Intent iCreateAccount = new Intent(this, SignUpActivity.class);
--- app/src/main/java/com/goodfellows/morssenger/SignUpActivity.java
+++ app/src/main/java/com/goodfellows/morssenger/SignUpActivity.java
@@ -1,16 +1,101 @@
 package com.goodfellows.morssenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth auth; //creates fire base Authentication object -Evans
+    private Button buttonSignUp;
+    private EditText nEmail, nPassword, nFirstName, nLastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
         getSupportActionBar().hide();
+
+        //Get Firebase auth instance -Evans
+        auth = FirebaseAuth.getInstance();
+
+        //set button/editText to respective IDs -Evans
+        buttonSignUp = (Button) findViewById(R.id.button_sign_up3);
+        nEmail = (EditText) findViewById(R.id.et_email);
+        nPassword = (EditText) findViewById(R.id.et_password);
+        nFirstName = (EditText) findViewById(R.id.et_first_name);
+        nLastName = (EditText) findViewById(R.id.et_last_name);
+
+        //
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //sets user input to strings
+                final String email = nEmail.getText().toString().trim();
+                final String password = nPassword.getText().toString().trim();
+                final String fname = nFirstName.getText().toString().trim();
+                final String lname = nLastName.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignUpActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+
+                                    addUserTofirebase(email, password, fname, lname);
+                                    startActivity(new Intent(SignUpActivity.this, ConversationsActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+    //sends new user information to firebase.
+    private void addUserTofirebase(String email, String password, String fname, String lname){
+
+        User user = new User();
+
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setFirstName(fname);
+        user.setLastName(lname);
+
+        auth.createUserWithEmailAndPassword(user.email, user.password);
     }
 }
