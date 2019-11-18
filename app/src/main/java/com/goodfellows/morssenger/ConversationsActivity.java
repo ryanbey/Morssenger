465a12dbd403faddc29a90208d75d0c93457efbc
Ryan <beycommaryan@gmail.com>
2019-11-17 21:37:05 -0700

    added recycler view to conversations activity with placeholder list items

--- app/src/main/java/com/goodfellows/morssenger/ConversationsActivity.java
+++ app/src/main/java/com/goodfellows/morssenger/ConversationsActivity.java
@@ -1,16 +1,54 @@
 package com.goodfellows.morssenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class ConversationsActivity extends AppCompatActivity {
+
+    private static final String TAG = "ConversationsActivity";
+
+    // Variables
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageURLs = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversations);
-
         Utils.greenStatusBar(this, R.color.colorMorseGreen); // Green status bar
+        Log.d(TAG, "onCreate: started");
+
+        initImageBitmaps();
+    }
+
+    private void initImageBitmaps() {
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps");
+        // We can add image urls in here for contact pictures
+
+        // App doesn't show anything without test images in here currently
+        mImageURLs.add("https://i.redd.it/snywd65vzt7y.jpg");
+        mNames.add("Yosemite");
+
+        mImageURLs.add("https://i.redd.it/bt2jpx8q9hu31.jpg");
+        mNames.add("City Sunset");
+
+        // This can be done in onCreate if we decide not to use images for contacts
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mNames, mImageURLs, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/goodfellows/morssenger/RecyclerViewAdapter.java
@@ -0,0 +1,109 @@
+package com.goodfellows.morssenger;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/*
+RECYCLER VIEW ADAPTER
+This class adapts the list item layout to something the recycler view can read and use
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    // Tag for log messages
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    // Default Constructor
+    public RecyclerViewAdapter(ArrayList<String> imageNames, ArrayList<String> images, Context context) {
+        mImageNames = imageNames;
+        mImages = images;
+        mContext = context;
+    }
+
+
+    /*
+    Recycles the ViewHolders and puts them into the positions they are supposed to be in
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+
+    /*
+    This class can change based on what you want the layout to look like
+    Right now it uses the images and shows a toast when you click on a list item
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+
+        Log.d(TAG, "onBindViewHolder: called");
+
+        // Loads images from the internet into our imageViews
+        // Glide is a library that lets us edit images more easily
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(holder.image);
+
+        holder.imageName.setText(mImageNames.get(position));
+
+        // Right now this shows a toast, CHANGE LATER TO GO TO MESSAGE FRAGMENT
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
+                Toast.makeText(mContext,mImageNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    /*
+    Tells RecyclerViewAdapter how many items are in the list so it knows how many to display
+     */
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+
+    // Handles different views with their descriptions
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+
+        }
+    }
+}
